1a71a8c17d9a8cd0981dfcd0b82df81c33f7613f
pry0cc <pry0cc@0x00sec.org>
2020-06-24 01:16:34 +0000

    added sudo on cp

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo cp reaperd /usr/bin/
 sudo chmod +x /usr/bin/reaperd
 
-cp reaperd.service /etc/systemd/system/reaperd.service
+sudo cp reaperd.service /etc/systemd/system/reaperd.service
 sudo chmod 664 /etc/systemd/system/reaperd.service
 
 sudo systemctl daemon-reload
