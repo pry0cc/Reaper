c4eb12f8f8801606aaa8707b5a51375ac53a45ff
pry0cc <pry0cc@0x00sec.org>
2020-06-24 12:13:29 +0000

    Added renewed install script features

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 sudo cp reaperd /usr/bin/
+sudo cp set-expiry.sh /usr/bin/
+sudo cp seconds-to-death.sh /usr/bin/
 sudo chmod +x /usr/bin/reaperd
+sudo chmod +x /usr/bin/seconds-to-death.sh
+sudo chmod +x /usr/bin/set-expiry.sh
 
 sudo cp reaperd.service /etc/systemd/system/reaperd.service
 sudo chmod 664 /etc/systemd/system/reaperd.service
 
 sudo systemctl daemon-reload
+sudo systemctl enable reaperd
 sudo systemctl start reaperd
+sudo set-expiry.sh "1000 years"
